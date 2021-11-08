6953292162d81fe14f90382eac7bbf368841cf92
Eduardo Kaique Freitas <eduardokaique.dev@gmail.com>
2021-11-07 23:49:32 -0300

    ajuste de porta de aplicação.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
 import { serverHttp } from "./app";
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 serverHttp.listen(port, () => console.log(`🚀 Server is run on PORT ${port}`));
